b6f33efd6a3ce488d11a313a35d241ce4b1e3f15
Nhat Tien <nhattienst577@gmail.com>
2022-02-19 14:39:12 +0700

    Update version react

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -61,7 +61,7 @@ class Header extends Component {
             </Collapse>
           </div>
         </Navbar>
-        <div className="Jumbotron">
+        <Jumbotron>
           <div className="container">
             <div className="row row-header">
               <div className="col-12 col-sm-6">
@@ -69,7 +69,7 @@ class Header extends Component {
               </div>
             </div>
           </div>
-        </div>
+        </Jumbotron>
       </div>
     );
   }
