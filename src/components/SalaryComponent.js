b874822b1a7e3aad8b417626383c70b418f725ef
Nhat Tien <nhattienst577@gmail.com>
2022-03-05 23:06:59 +0700

    add salary

--- src/components/SalaryComponent.js
+++ src/components/SalaryComponent.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import {
   Card,
   CardTitle,
@@ -7,54 +7,53 @@ import {
   BreadcrumbItem,
 } from "reactstrap";
 import { Link } from "react-router-dom";
-const basicSalary = 3000000;
-const overTimeSalary = 200000;
 
-class Salary extends Component {
-  render() {
-    const paysalary = this.props.staffs.map((salary) => {
-      return (
-        <div
-          key={salary.id}
-          className="col-md-4 col-sm-6 col-12"
-          style={{ padding: "15px 15px" }}
-        >
-          <Card>
-            <CardTitle>{salary.name}</CardTitle>
-            <ul style={{ padding: "0 15px" }}>
-              <CardText>Mã Nhân Viên: {salary.id}</CardText>
-              <CardText>Hệ Số Lương: {salary.salaryScale}</CardText>
-              <CardText>Số Giờ Làm Thêm: {salary.overTime}</CardText>
-
-              <div className="bg-secondary">
-                <CardText className="salary">
-                  Lương: {basicSalary + salary.overTime * overTimeSalary}
-                </CardText>
-              </div>
-            </ul>
-          </Card>
-        </div>
-      );
-    });
-
+function RenderSalary({ salarys }) {
+  console.log("salary: " + salarys);
+  const ListSalary = salarys.salarys.map((salary) => {
     return (
-      <div className="container">
-        <div className="row">
-          <Breadcrumb>
-            <BreadcrumbItem>
-              <Link to="/nhan-vien">Nhân Viên</Link>
-            </BreadcrumbItem>
-            <BreadcrumbItem active>Bảng Lương</BreadcrumbItem>
-          </Breadcrumb>
-          <div className="col-12">
-            <h3>Bảng Lương</h3>
-            <hr />
+      <Card
+        key={salary.id}
+        className="col-md-4 col-sm-6 col-12"
+        style={{ padding: "15px 15px" }}
+      >
+        <CardTitle>{salary.name}</CardTitle>
+        <ul style={{ padding: "0 15px" }}>
+          <CardText>Mã Nhân Viên: {salary.id}</CardText>
+          <CardText>Hệ Số Lương: {salary.salaryScale}</CardText>
+          <CardText>Số Giờ Làm Thêm: {salary.overTime}</CardText>
+          <div className="bg-secondary">
+            <CardText>Lương: {salary.salary}</CardText>
           </div>
-        </div>
-        <div className="row">{paysalary}</div>
-      </div>
+        </ul>
+      </Card>
     );
-  }
+  });
+  return <div className="row">{ListSalary}</div>;
 }
+
+const Salary = (props) => {
+  return (
+    <div className="container">
+      <div className="row">
+        <Breadcrumb>
+          <BreadcrumbItem>
+            <Link to="/nhan-vien">Nhân Viên</Link>
+          </BreadcrumbItem>
+          <BreadcrumbItem active>Bảng Lương</BreadcrumbItem>
+        </Breadcrumb>
+      </div>
+      <div className="row">
+        <div className="col-12">
+          <h3>Bảng Lương</h3>
+          <hr />
+        </div>
+      </div>
+      <div className="row">
+        <RenderSalary salarys={props.salarys} />
+      </div>
+    </div>
+  );
+};
 
 export default Salary;
