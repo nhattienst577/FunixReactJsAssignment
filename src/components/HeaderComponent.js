cefe5c54eadb1f12078e958dce00e16cafaa8f48
Nhat Tien <nhattienst577@gmail.com>
2022-03-29 22:15:37 +0700

    update

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -34,7 +34,7 @@ class Header extends Component {
             <NavbarToggler onClick={this.toggleNav} />
             <NavbarBrand className="mr-auto" href="/">
               <img
-                src="asset/images/logo.png"
+                src="assets/images/logo.png"
                 height="30"
                 width="41"
                 alt="Ứng dụng quản lý nhân sự v1.0"
